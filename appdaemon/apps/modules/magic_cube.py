1b7b21c9169de303b427f53d1bdabeb3ad120df7
Laurent <lbouriez@yahoo.fr>
2019-11-09 18:49:49 -0500

    Update semaine

--- appdaemon/apps/modules/magic_cube.py
+++ appdaemon/apps/modules/magic_cube.py
@@ -16,7 +16,7 @@ class Action(str, Enum):
     FREE_FALL = 'free_fall'
     ROTATE = 'rotate'
 
-class CubeControl(hass.Hass):
+class MagicCube(hass.Hass):
     
     def initialize(self):
         self.event_name = "app_daemon_magic_cube"
---
+++ appdaemon/apps/modules/symfonisk_sonos.py
@@ -0,0 +1,76 @@
+import appdaemon.plugins.hass.hassapi as hass
+import time
+
+class SymfoniskSonos(hass.Hass):
+    """ [summary]
+        [event] {string} -- Event name that will be fired (ex: deconz_event)
+        [remotes] {list} -- List of the symfonisk remote id in DeconZ (ex: - symfonisk_sound_controller)
+        [sonos] {list} -- List of the sonos media player to control (ex: - media_player.bathroom)
+        [sources] {list} -- Name of the source to play when double clicking (ex: - 'Sonos favoris name')
+        [play_shuffle] {boolean} -- Shuffle play the playlist
+    """
+
+    def initialize(self):
+        self.handle_params()
+        if 'event' in self.args:
+            self.listen_event(self.handle_event, self.args['event'])
+
+    def handle_params(self):
+        self.log(self.args)
+        self.event_name = "app_daemon_symfonisk"
+        self.sonos = self.args.get('sonos')
+        self.sources = self.args.get('sources')
+        self.remotes = self.args.get('remotes')
+        self.play_shuffle = self.args.get('play_shuffle', False)
+
+        self.initial_source = 0
+        self.volume_change = False
+
+    def handle_source(self):
+        selected_source = self.sources[self.initial_source]
+        self.initial_source += 1
+        if self.initial_source >= len(self.sources):
+            self.initial_source = 0
+        return selected_source
+
+    def handle_volume(self, kwargs):
+        self.log("Change volume loop " + kwargs["way"])
+        self.call_service("media_player/volume_" + kwargs["way"], entity_id = self.sonos)
+        if self.volume_change:
+            self.run_in(self.handle_volume, 0.2, way = kwargs["way"])
+        
+    def handle_event(self, event_name, data, kwargs):
+        remote_id = data['id']
+
+        if remote_id in self.remotes:
+            if data['event'] == 1002:
+                self.log('Button simple click - toggle')
+                self.call_service("media_player/media_play_pause", entity_id = self.sonos)
+                self.fire_event(self.event_name, entity_id = self.sonos, state="click1")
+            elif data['event'] == 1004:
+                self.log('Button double click - next track')
+                self.call_service("media_player/media_next_track", entity_id = self.sonos)
+                self.fire_event(self.event_name, entity_id = self.sonos, state="click2")
+            elif data['event'] == 1005:
+                self.log('Button triple click - choose source')
+                if len(self.sonos) > 1:
+                    self.call_service("sonos/join", entity_id = self.sonos, master = self.sonos[0])
+                self.call_service("media_player/shuffle_set", entity_id = self.sonos, shuffle = self.play_shuffle)
+                self.call_service("media_player/select_source", entity_id = self.sonos, source = self.handle_source())
+                self.fire_event(self.event_name, entity_id = self.sonos, state="click3")
+            elif data['event'] == 3001:
+                self.log('Button volume up')
+                self.volume_change = True
+                self.handle_volume({'way': 'up'})
+                self.fire_event(self.event_name, entity_id = self.sonos, state="volup")
+            elif data['event'] == 2001:
+                self.log('Button volume down')
+                self.volume_change = True
+                self.handle_volume({'way': 'down'})
+                self.fire_event(self.event_name, entity_id = self.sonos, state="voldown")
+            elif data['event'] in [2003, 3003]: 
+                self.log('Button volume stop')
+                self.volume_change = False
+                self.fire_event(self.event_name, entity_id = self.sonos, state="volstop")
+            else:
+                self.log('Unkown action: ' + data['event'])
